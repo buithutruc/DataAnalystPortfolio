9f5c153c3720020013bfd69838b9d8ffb882b363
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-02-23 10:01:12 -0500

    got date automatically across the website

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-function getCurrentDate() {
+const getCurrentDate = () => {
   let today = new Date();
   let day = today.getDate();
   let month = today.getMonth() + 1;
@@ -14,10 +14,19 @@ function getCurrentDate() {
 
   let todayTxt = monthTxt + " " + day + ", " + year;
 
-  let currentDate = document.querySelectorAll(".now");
+  let currentDate = document.querySelectorAll(".date");
   for (let i = 0; i < currentDate.length; i++) {
     currentDate[i].innerText = todayTxt;
   }
-}
+};
 
 getCurrentDate();
+
+const sendMail = () => {
+  const mailAdress = "someone@example.com";
+  const ccRecipients = "vinamiincanada@gmail.com";
+  const subject = encodeURIComponent("This is my subject");
+  const body = encodeURIComponent(document.getElementById("myText").value);
+  const link = `mailto:${mailAdress}?cc=${ccRecipients}&subject=${subject}&body=${body}`;
+  window.location.href = link;
+};
