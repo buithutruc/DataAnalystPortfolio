04a4a758697e186a831650243dd929436f299276
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-02-23 08:50:04 -0500

    changed the date text into current date using JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
+function getCurrentDate() {
+  let today = new Date();
+  let day = today.getDate();
+  let month = today.getMonth() + 1;
+  let year = today.getFullYear();
+  let monthTxt;
 
+  switch (month) {
+    case 1:
+      monthTxt = "January";
+    case 2:
+      monthTxt = "February";
+  }
+
+  let todayTxt = monthTxt + " " + day + ", " + year;
+
+  let currentDate = document.querySelectorAll(".now");
+  for (let i = 0; i < currentDate.length; i++) {
+    currentDate[i].innerText = todayTxt;
+  }
+}
+
+getCurrentDate();
